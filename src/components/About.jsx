061404122241d98fbf187ee9e069b23155a6ed90
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-01-04 13:10:22 +0530

    about update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 export const About = () => {
-
   const handleContact = (e) => {
     e.preventDefault();
     alert("Your message successfully send ✅");
-  }
+  };
 
   return (
     <>
@@ -13,25 +12,39 @@ export const About = () => {
         <div className="about">
           <h2>About</h2>
           <p style={{ marginTop: "1rem" }}>
-            I am Mohanapriyan. This website is my small projecṭ in react js
+            <b>
+              I am Mohanapriyan. This website is my small project in react js
+            </b>
           </p>
           <p
             style={{
-              fontSize: "14px",
-              marginTop: "3rem",
+              marginTop: "1rem",
               marginBottom: "30px",
+              textAlign: "start",
+              padding: "10px",
             }}
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
-            perferendis tempora aspernatur deleniti consequuntur dicta,
-            consequatur cum iusto. Omnis dolore architecto cumque qui illum ut
-            incidunt minima ratione nam doloribus?
+            Welcome to PostUp, the platform designed to make sharing and
+            managing your messages simple and effective. Built with cutting-edge
+            technology using local storage and APIs, PostUp offers a fast and
+            secure way to post, store, and interact with messages. <br />
+            <br />
+            At PostUp, we prioritize user experience and efficiency. Our
+            platform ensures your messages are accessible and well-organized,
+            whether you're sharing ideas, collaborating with others, or keeping
+            personal notes.
+            <br />
+            <br /> With PostUp, your thoughts and communications are always at
+            your fingertips. Join us in creating a space where sharing is
+            seamless and connections are stronger.
+            <br />
+            <br /> Thank you for choosing PostUp—where every message matters.
           </p>
         </div>
       </center>
       <center>
         <div className="contact">
-        <h2>Contact Us</h2>
+          <h2>Contact Us</h2>
           <form onSubmit={handleContact}>
             <label>Name</label>
             <input type="text" required />
@@ -40,7 +53,9 @@ export const About = () => {
             <label>Message</label>
             <textarea required></textarea>
             <div className="btn">
-              <button className="contact-btn" type="submit">send</button>
+              <button className="contact-btn" type="submit">
+                send
+              </button>
             </div>
           </form>
         </div>
